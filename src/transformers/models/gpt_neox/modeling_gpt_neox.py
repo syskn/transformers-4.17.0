64dd7190d7a2a32e2d141dde0fa29edfc8283b9a
syskn <bit192@gmail.com>
2022-12-25 04:10:39 +0900

    Update modeling_gpt_neox.py

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -44,7 +44,7 @@ def fixed_pos_embedding(x, seq_dim=1, seq_len=None):
         seq_len = x.shape[seq_dim]
     inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2) / dim))
     # sinusoid_inp = torch.einsum("i , j -> i j", torch.arange(seq_len), inv_freq).to(x.device).float()
-    sinusoid_inp = torch.einsum("i , j -> i j", torch.arange(seq_len), inv_freq, device=x.device).float()
+    sinusoid_inp = torch.einsum("i , j -> i j", torch.arange(seq_len, device=x.device), inv_freq, device=x.device).float()
     sinusoid_inp = torch.cat((sinusoid_inp, sinusoid_inp), dim=-1)
     return torch.sin(sinusoid_inp), torch.cos(sinusoid_inp)
 
@@ -195,15 +195,15 @@ class GPTNeoXAttention(nn.Module):
             q_pass = query[:, :, :, self.rotary_dim :]
 
             sincos = fixed_pos_embedding(k_rot, 1, seq_len=seq_len)
-            k_rot = apply_rotary_pos_emb(k_rot, sincos.to('cpu'), offset=offset)
-            q_rot = apply_rotary_pos_emb(q_rot, sincos.to('cpu'), offset=offset)
+            k_rot = apply_rotary_pos_emb(k_rot, sincos, offset=offset)
+            q_rot = apply_rotary_pos_emb(q_rot, sincos, offset=offset)
 
             key = torch.cat([k_rot, k_pass], dim=-1)
             query = torch.cat([q_rot, q_pass], dim=-1)
         else:
             sincos = fixed_pos_embedding(key, 1, seq_len=seq_len)
-            key = apply_rotary_pos_emb(key, sincos.to('cpu'), offset=offset)
-            query = apply_rotary_pos_emb(query, sincos.to('cpu'), offset=offset)
+            key = apply_rotary_pos_emb(key, sincos, offset=offset)
+            query = apply_rotary_pos_emb(query, sincos, offset=offset)
 
         key = key.permute(0, 2, 1, 3)
         query = query.permute(0, 2, 1, 3)
