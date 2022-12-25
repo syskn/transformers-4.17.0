2d35fb334a9b19b24ee913e100cdd169c8e3dd75
syskn <bit192@gmail.com>
2022-12-25 17:29:07 +0900

    Update modeling_gpt_neox.py

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -129,7 +129,8 @@ class GPTNeoXAttention(nn.Module):
     ):
 
         # compute causal mask from causal mask buffer
-        value = value.to(torch.float32)
+        query = query.to(torch.float16)
+        key = key.to(torch.float16)
 
         key = key.permute(0, 2, 1, 3)
         query = query.permute(0, 2, 1, 3)
