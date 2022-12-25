dc373b30f1c642742a7812e2a61be3dd420ae629
syskn <bit192@gmail.com>
2022-12-26 01:27:19 +0900

    Update modeling_gpt_neox.py

--- src/transformers/models/gpt_neox/modeling_gpt_neox.py
+++ src/transformers/models/gpt_neox/modeling_gpt_neox.py
@@ -130,9 +130,10 @@ class GPTNeoXAttention(nn.Module):
         # compute causal mask from causal mask buffer
         key = key.transpose(1, 2)
         query = query.transpose(1, 2)
+        value = value.transpose(1, 2)
         value = value.to(torch.float32)
 
-        y = xops.memory_efficient_attention(query, key, value, attn_bias=xops.LowerTriangularMask(), scale=self.scale_attn).to(torch.float16)
+        y = xops.memory_efficient_attention(query, key, value, attn_bias=xops.LowerTriangularMask()).to(torch.float16)
         return y, None
 
         query_length, key_length = query.size(-2), key.size(-2)
